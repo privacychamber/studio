d51a419d1f81e01fee2d26db09b672e2e416c025
privacychamber <privacychamber@users.noreply.github.com>
2026-04-29 22:35:04 +0530

    Fix react-day-picker calendar types for v9

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,12 +54,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Chevron: ({ className, ...props }) => {
+          if (props.orientation === "left") {
+            return <ChevronLeft className={cn("h-4 w-4", className)} {...(props as any)} />
+          }
+          return <ChevronRight className={cn("h-4 w-4", className)} {...(props as any)} />
+        },
       }}
       {...props}
     />
